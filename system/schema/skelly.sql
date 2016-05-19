01ad98b14e599c857ff6d594906e17b5aef67aa8
William Byrne <will_byrne56@hotmail.com>
2016-05-19 19:02:05 +0100

    updated schema

--- system/schema/skelly.sql
+++ system/schema/skelly.sql
@@ -1,16 +1,28 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               10.1.9-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win32
--- HeidiSQL Version:             9.3.0.4984
+-- Server version:               5.6.12-log - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL version:             7.0.0.4053
+-- Date/time:                    2016-05-19 18:58:04
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
 
--- Dumping structure for table skelly.backend_users
+-- Dumping structure for table ironworkshop.about_us
+CREATE TABLE IF NOT EXISTS `about_us` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `text` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ironworkshop.about_us: ~0 rows (approximately)
+/*!40000 ALTER TABLE `about_us` DISABLE KEYS */;
+/*!40000 ALTER TABLE `about_us` ENABLE KEYS */;
+
+
+-- Dumping structure for table ironworkshop.backend_users
 CREATE TABLE IF NOT EXISTS `backend_users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_name` varchar(255) NOT NULL,
@@ -24,45 +36,182 @@ CREATE TABLE IF NOT EXISTS `backend_users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
--- Dumping data for table skelly.backend_users: ~3 rows (approximately)
+-- Dumping data for table ironworkshop.backend_users: ~0 rows (approximately)
 /*!40000 ALTER TABLE `backend_users` DISABLE KEYS */;
 INSERT INTO `backend_users` (`id`, `user_name`, `user_pass`, `user_email`, `salt`, `display_name`, `is_super`, `created`, `modified`) VALUES
 	(1, 'creative', '44e32df05cb0bc59d45534e152c54e4dcf5cfc1b192fe2d2396971a5c4858b36', 'william@outputdigital.com', 1435578356, 'William', 1, '2016-01-11 15:13:56', '0000-00-00 00:00:00');
 /*!40000 ALTER TABLE `backend_users` ENABLE KEYS */;
 
 
--- Dumping structure for table skelly.frontend_users
-CREATE TABLE IF NOT EXISTS `frontend_users` (
+-- Dumping structure for table ironworkshop.category
+CREATE TABLE IF NOT EXISTS `category` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(255) DEFAULT NULL,
-  `surname` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `email_verified` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `password` varchar(255) DEFAULT NULL,
-  `salt` int(11) unsigned DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
   `is_active` tinyint(1) unsigned NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=542 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table skelly.frontend_users: ~4 rows (approximately)
-/*!40000 ALTER TABLE `frontend_users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `frontend_users` ENABLE KEYS */;
+-- Dumping data for table ironworkshop.category: ~0 rows (approximately)
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
 
 
--- Dumping structure for table skelly.frontend_users_pw_recovery
-CREATE TABLE IF NOT EXISTS `frontend_users_pw_recovery` (
+-- Dumping structure for table ironworkshop.contact_us
+CREATE TABLE IF NOT EXISTS `contact_us` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `users_id` int(11) unsigned NOT NULL,
-  `security_key` varchar(255) NOT NULL,
-  `exp_date` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_frontend_users_pw_recovery_frontend_users` (`users_id`),
-  CONSTRAINT `FK_frontend_users_pw_recovery_frontend_users` FOREIGN KEY (`users_id`) REFERENCES `frontend_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `facebook` varchar(255) NOT NULL,
+  `instagram` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `text` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table skelly.frontend_users_pw_recovery: ~0 rows (approximately)
-/*!40000 ALTER TABLE `frontend_users_pw_recovery` DISABLE KEYS */;
-/*!40000 ALTER TABLE `frontend_users_pw_recovery` ENABLE KEYS */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+-- Dumping data for table ironworkshop.contact_us: ~0 rows (approximately)
+/*!40000 ALTER TABLE `contact_us` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contact_us` ENABLE KEYS */;
+
+
+-- Dumping structure for table ironworkshop.emails
+CREATE TABLE IF NOT EXISTS `emails` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ironworkshop.emails: ~0 rows (approximately)
+/*!40000 ALTER TABLE `emails` DISABLE KEYS */;
+/*!40000 ALTER TABLE `emails` ENABLE KEYS */;
+
+
+-- Dumping structure for table ironworkshop.faq
+CREATE TABLE IF NOT EXISTS `faq` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `question` varchar(255) NOT NULL,
+  `answer` varchar(255) NOT NULL,
+  `sort` int(11) unsigned NOT NULL DEFAULT '0',
+  `is_active` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ironworkshop.faq: ~0 rows (approximately)
+/*!40000 ALTER TABLE `faq` DISABLE KEYS */;
+/*!40000 ALTER TABLE `faq` ENABLE KEYS */;
+
+
+-- Dumping structure for table ironworkshop.gallery
+CREATE TABLE IF NOT EXISTS `gallery` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `video` varchar(255) DEFAULT NULL,
+  `sort` int(11) unsigned NOT NULL DEFAULT '0',
+  `is_active` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ironworkshop.gallery: ~0 rows (approximately)
+/*!40000 ALTER TABLE `gallery` DISABLE KEYS */;
+/*!40000 ALTER TABLE `gallery` ENABLE KEYS */;
+
+
+-- Dumping structure for table ironworkshop.news
+CREATE TABLE IF NOT EXISTS `news` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) unsigned DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `text` text NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_news_category` (`category_id`),
+  CONSTRAINT `FK_news_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ironworkshop.news: ~0 rows (approximately)
+/*!40000 ALTER TABLE `news` DISABLE KEYS */;
+/*!40000 ALTER TABLE `news` ENABLE KEYS */;
+
+
+-- Dumping structure for table ironworkshop.results
+CREATE TABLE IF NOT EXISTS `results` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `trainer_id` int(11) unsigned DEFAULT NULL,
+  `image` varchar(255) NOT NULL,
+  `text` text NOT NULL,
+  `sort` int(11) unsigned NOT NULL DEFAULT '0',
+  `is_active` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `FK__trainers_results` (`trainer_id`),
+  CONSTRAINT `FK__trainers_results` FOREIGN KEY (`trainer_id`) REFERENCES `trainers` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ironworkshop.results: ~0 rows (approximately)
+/*!40000 ALTER TABLE `results` DISABLE KEYS */;
+/*!40000 ALTER TABLE `results` ENABLE KEYS */;
+
+
+-- Dumping structure for table ironworkshop.store
+CREATE TABLE IF NOT EXISTS `store` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `price` float(11,2) unsigned NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `text` text NOT NULL,
+  `sort` int(11) unsigned NOT NULL DEFAULT '0',
+  `is_active` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ironworkshop.store: ~0 rows (approximately)
+/*!40000 ALTER TABLE `store` DISABLE KEYS */;
+/*!40000 ALTER TABLE `store` ENABLE KEYS */;
+
+
+-- Dumping structure for table ironworkshop.store_images
+CREATE TABLE IF NOT EXISTS `store_images` (
+  `store_id` int(11) unsigned NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  KEY `FK__store` (`store_id`),
+  CONSTRAINT `FK__store` FOREIGN KEY (`store_id`) REFERENCES `store` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ironworkshop.store_images: ~0 rows (approximately)
+/*!40000 ALTER TABLE `store_images` DISABLE KEYS */;
+/*!40000 ALTER TABLE `store_images` ENABLE KEYS */;
+
+
+-- Dumping structure for table ironworkshop.trainers
+CREATE TABLE IF NOT EXISTS `trainers` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `text` text NOT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `website` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ironworkshop.trainers: ~0 rows (approximately)
+/*!40000 ALTER TABLE `trainers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trainers` ENABLE KEYS */;
+
+
+-- Dumping structure for table ironworkshop.trainer_images
+CREATE TABLE IF NOT EXISTS `trainer_images` (
+  `trainer_id` int(11) unsigned NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `text` text NOT NULL,
+  `sort` int(11) unsigned NOT NULL DEFAULT '0',
+  KEY `FK__trainers` (`trainer_id`),
+  CONSTRAINT `FK__trainers` FOREIGN KEY (`trainer_id`) REFERENCES `trainers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table ironworkshop.trainer_images: ~0 rows (approximately)
+/*!40000 ALTER TABLE `trainer_images` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trainer_images` ENABLE KEYS */;
+/*!40014 SET FOREIGN_KEY_CHECKS=1 */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
